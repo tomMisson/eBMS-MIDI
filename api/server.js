0c809455b1454184473bbe4ca9c3fdcc1d69c87f
tomMisson <11tmisson@gmail.com>
2019-12-28 21:13:40 +0000

    Containerised all services, added mongo container, api returns hash  token with correct credentials

---
+++ api/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const bodyParser = require('body-parser')
+const hash = require('hash.js')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT
+
+var auth = express()
+var api = express()
+var net = express()
+
+app.use('/auth', auth)
+app.use('/api', api)
+app.use('/network', net)
+
+auth.get('/', (req, res) => {
+    res.send("POST credentials to create access token");
+})
+
+auth.post('/', (req, res) => {
+    const data = req.body
+    
+    if(data.username === process.env.USRNAME && data.password === process.env.PSWD)
+    {
+        let token = hash.sha256().update(req.headers['x-forwarded-for'] || req.connection.remoteAddress).digest('hex')
+        res.json({"token":token})
+    }
+    else
+    {
+        res.send(401)
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
