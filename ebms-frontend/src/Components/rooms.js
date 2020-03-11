ea47d9723d7bd3abf729724eefbd461a3369563c
tomMisson <11tmisson@gmail.com>
2020-03-11 22:29:59 +0000

    Added funamentals of room need to have delete and add options and fetch data from API

--- ebms-frontend/src/Components/rooms.js
+++ ebms-frontend/src/Components/rooms.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Rooms extends Component {
     state = { rooms: [
-        {"name": "demo",
-        "devices": ["test1", "test2"]
+        {   
+            "id": 0,
+            "name": "Demo",
+            "devices": [{"name": "test1"},{"name": "test2"}]
         }
     ] }
 
@@ -28,9 +31,13 @@ class Rooms extends Component {
             <main>
                 {
                 this.state.rooms.map( (room) => 
-                    <section>
+                    <section key={room.id} className="room">
                         <h2>{room.name}</h2>
-                        <p>{room.devices}</p>
+                        <br></br>
+                        {
+                        room.devices.map((device) => 
+                            <Link to="/devices" className="deviceName">{device.name}</Link>
+                        )}
                     </section>
                 )}
             </main>
